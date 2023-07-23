5211f2fbff3d679c71949eac06824e4c4b4f6834
Michael Dratch <michael.s.dratch@gmail.com>
2023-07-23 14:17:02 -0400

    created get all voters end point

--- VOTER_API/main.go
+++ VOTER_API/main.go
@@ -6,6 +6,9 @@ import (
 	"os"
 
 	"voter-api/api"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
 )
 
 // Global variables to hold the command line flags to drive the todo CLI
@@ -15,43 +18,14 @@ var (
 	portFlag uint
 )
 
-// processCmdLineFlags parses the command line flags for our CLI
-//
-// TODO: This function uses the flag package to parse the command line
-//		 flags.  The flag package is not very flexible and can lead to
-//		 some confusing code.
-
-//			 REQUIRED:     Study the code below, and make sure you understand
-//						   how it works.  Go online and readup on how the
-//						   flag package works.  Then, write a nice comment
-//				  		   block to document this function that highights that
-//						   you understand how it works.
-//
-//			 EXTRA CREDIT: The best CLI and command line processor for
-//						   go is called Cobra.  Refactor this function to
-//						   use it.  See github.com/spf13/cobra for information
-//						   on how to use it.
-//
-//	 YOUR ANSWER: <GOES HERE>
 func processCmdLineFlags() {
 
-	//Note some networking lingo, some frameworks start the server on localhost
-	//this is a local-only interface and is fine for testing but its not accessible
-	//from other machines.  To make the server accessible from other machines, we
-	//need to listen on an interface, that could be an IP address, but modern
-	//cloud servers may have multiple network interfaces for scale.  With TCP/IP
-	//the address 0.0.0.0 instructs the network stack to listen on all interfaces
-	//We set this up as a flag so that we can overwrite it on the command line if
-	//needed
 	flag.StringVar(&hostFlag, "h", "0.0.0.0", "Listen on all interfaces")
 	flag.UintVar(&portFlag, "p", 1080, "Default Port")
 
 	flag.Parse()
 }
 
-// main is the entry point for our todo API application.  It processes
-// the command line flags and then uses the db package to perform the
-// requested operation
 func main() {
 	processCmdLineFlags()
 	r := gin.Default()
@@ -63,22 +37,14 @@ func main() {
 		os.Exit(1)
 	}
 
-	r.GET("/todo", apiHandler.ListAllTodos)
-	r.POST("/todo", apiHandler.AddToDo)
-	r.PUT("/todo", apiHandler.UpdateToDo)
-	r.DELETE("/todo", apiHandler.DeleteAllToDo)
-	r.DELETE("/todo/:id", apiHandler.DeleteToDo)
-	r.GET("/todo/:id", apiHandler.GetToDo)
-
-	r.GET("/crash", apiHandler.CrashSim)
-	r.GET("/health", apiHandler.HealthCheck)
-
-	//We will now show a common way to version an API and add a new
-	//version of an API handler under /v2.  This new API will support
-	//a path parameter to search for todos based on a status
-	v2 := r.Group("/v2")
-	v2.GET("/todo", apiHandler.ListSelectTodos)
-
+	r.GET("/voters", apiHandler.ListAllVoters)
+	//r.GET("/voters/:id", apiHandler.GetVoter)
+	//r.POST("/voters/:id", apiHandler.AddVoter)
+	//r.GET("/voters/:id/polls", apiHandler.GetVoterHistory)
+	//r.GET("/voters/:id/polls/:pollid", apiHandler.GetVoterPoll)
+	//r.POST("/voters/:id/polls/:pollid", apiHandler.AddVoterPoll)
+	//r.GET("/voters/health", apiHandler.HealthCheck)
+	
 	serverPath := fmt.Sprintf("%s:%d", hostFlag, portFlag)
 	r.Run(serverPath)
 }
