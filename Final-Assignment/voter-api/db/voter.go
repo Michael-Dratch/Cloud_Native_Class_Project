86bde26473ebbc8b644c90fd0b9d9d6502786e82
Michael Dratch <michael.s.dratch@gmail.com>
2023-09-03 16:03:23 -0400

    created voter API no hypermedia

--- Final-Assignment/voter-api/db/voter.go
+++ Final-Assignment/voter-api/db/voter.go
@@ -7,7 +7,6 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"time"
 
 	"github.com/go-redis/redis/v8"
 	"github.com/nitishm/go-rejson/v4"
@@ -17,7 +16,6 @@ const (
 	RedisNilError = "redis: nil"
 	RedisDefaultLocation = "0.0.0.0:6379"
 	RedisVoterKeyPrefix = "voter:"
-	RedisVoterPollKeyPrefix = "voterPoll:"
 )
 
 type cache struct {
@@ -26,16 +24,10 @@ type cache struct {
 	context context.Context
 }
 
-type VoterPoll struct {
-	PollID uint
-	VoteDate time.Time
-}
-
 type Voter struct {
 	VoterID uint
 	FirstName string
 	LastName string
-	VoteHistory []VoterPoll
 }
 
 type VoterData struct {
@@ -89,9 +81,6 @@ func redisVoterKeyFromId(id int) string {
 	return fmt.Sprintf("%s%d", RedisVoterKeyPrefix, id)
 }
 
-func redisVoterPollKeyFromId(id int) string {
-	return fmt.Sprintf("%s%d", RedisVoterPollKeyPrefix, id)
-}
 
 func (v *VoterData) getVoterFromRedis(key string, voter *Voter) error {
 	voterObject, err := v.jsonHelper.JSONGet(key, ".")
@@ -112,19 +101,9 @@ func NewVoter(voterID uint, firstName string, lastName string) (*Voter, error){
 		VoterID: voterID,
 		FirstName: firstName,
 		LastName: lastName,
-		VoteHistory: make ([]VoterPoll, 0),
 	}
 
 	return voter, nil
-}
-
-func NewVoterPoll(pollID uint) (*VoterPoll, error){
-	voterPoll := &VoterPoll{
-		PollID: pollID,
-		VoteDate: time.Now(),
-	}
-
-	return voterPoll, nil
 }
 
 
@@ -197,9 +176,6 @@ func removeZeroValuesFromUpdateData(oldData Voter, updateData Voter) Voter {
 	if updateData.LastName == "" {
 		updateData.LastName = oldData.LastName
 	}
-	if updateData.VoteHistory == nil {
-		updateData.VoteHistory = oldData.VoteHistory
-	}
 
 	return updateData
 }
@@ -216,94 +192,3 @@ func (v *VoterData) DeleteVoter(voterID uint) error {
 
 	return nil
 }
-
-func (v *VoterData) GetVoterHistory(voterID uint) ([]VoterPoll, error){
-	voter, err := v.GetVoter(voterID)
-	if err != nil {
-		return make([]VoterPoll, 0) , errors.New("Voter ID does not exist")
-	}
-	
-	return voter.VoteHistory, nil
-}
-
-func (v *VoterData) GetVoterPoll(voterID uint, pollID uint) (VoterPoll, error){
-	voter, err := v.GetVoter(voterID)
-	if err != nil {
-		return VoterPoll{} , errors.New("Voter ID does not exist")
-	} 
-
-	for _, poll := range voter.VoteHistory{
-		if poll.PollID == pollID{
-			return poll, nil
-		}
-	}
-	log.Println("Error: Poll ID does not exist for this voter")
-	return VoterPoll{}, errors.New("Poll ID does not exist for this voter")
-}
-
-func (v *VoterData) DoesVoterPollExist(voterID uint, pollID uint) bool {
-	_, err := v.GetVoterPoll(voterID, pollID)
-	if err == nil { return true } 
-	return false 
-}
-
-func (v *VoterData) AddVoterPoll(voterID uint, newPoll VoterPoll) error{
-	voter, err := v.GetVoter(voterID)
-	if err != nil {
-		return errors.New("Voter ID does not exist")
-	} 
-
-	for _, poll := range voter.VoteHistory{
-		if newPoll.PollID == poll.PollID{
-			return errors.New("Poll ID already exists for this voter")
-		}
-	}
-
-	voter.VoteHistory = append(voter.VoteHistory, newPoll)
-	v.UpdateVoter(voterID, voter)
-	return nil
-}
-
-func (v *VoterData) UpdateVoterPoll(voterID uint, pollID uint, updateData VoterPoll) error{
-	
-	oldData,err := v.GetVoterPoll(voterID, pollID)
-	if err != nil {
-		return errors.New("Error: Voter poll does not exist")
-	}
-
-	updateData = removeZeroValuesFromPollUpdateData(oldData, updateData)
-	voter,_ := v.GetVoter(voterID)
-	for index,poll := range voter.VoteHistory{
-		if poll.PollID == pollID{
-			voter.VoteHistory[index] = updateData
-			break
-		}
-	}
-	v.UpdateVoter(voterID, voter)
-	return nil
-}
-
-func removeZeroValuesFromPollUpdateData(oldData VoterPoll, updateData VoterPoll) VoterPoll{
-	if updateData.VoteDate.IsZero() {
-		updateData.VoteDate = oldData.VoteDate
-	}
-	return updateData
-}
-
-func (v *VoterData) DeleteVoterPoll(voterID uint, pollID uint) error{
-	voter, err := v.GetVoter(voterID)
-	if err != nil {
-		return errors.New("Voter does not exists")
-	}
-
-	for index,poll := range voter.VoteHistory{
-		if poll.PollID == pollID{
-			voter.VoteHistory = append(voter.VoteHistory[:index], voter.VoteHistory[index+1:]... )
-			break
-		}
-	}
-
-	v.UpdateVoter(voterID, voter)
-	return nil
-}
-
